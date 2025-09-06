b198850eae84a7292e210be2a0ff9af6df4451ce
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-09-06 18:18:56 -0500

    Update Record-bytes.js

--- Record-bytes.js
+++ Record-bytes.js
@@ -1,1 +1,15 @@
-
+(() => {
+  const Q = fn => {
+      try {
+        return fn?.()
+      } catch {}
+    };
+  for (const record of [Request, Response, Blob]) {
+    (() => {
+      record.prototype.bytes ??= Object.setPrototypeOf(async function bytes() {
+        return new Uint8Array(await this.arrayBuffer());
+      }, Uint8Array);
+    })();
+  };
+})();
+    
