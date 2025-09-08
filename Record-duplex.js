bf063c0138112e9061f67b947b83962353233bc5
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-09-07 20:33:13 -0500

    Update Record-duplex.js

--- Record-duplex.js
+++ Record-duplex.js
@@ -1,27 +1,11 @@
 (()=>{
     if (!typeof Request || !typeof Response || !typeof ReadableStream) return;
-    const makeStringer = str => {
-        const stringer = () => str;
-        ['valueOf', 'toString', 'toLocaleString', Symbol.toPrimitive].forEach(x => {
-            stringer[x] = stringer;
+    for(const record of [Request, Response, ReadableStream, Blob]){
+        Object.defineProperty(record.prototype, 'duplex', {
+            value: 'half',
+            configurable: true,
+            writable: true,
+            enumerable: false,
         });
-        stringer[Symbol.toStringTag] = str;
-        return stringer;
-    };
-    const setStrings = (obj, name) => {
-        for (const str of ['toString', 'toLocaleString', Symbol.toStringTag]) {
-            Object.defineProperty(obj, str, {
-                value: makeStringer(`function ${obj.name}() { [polyfill code] }`),
-                configurable: true,
-                writable: true,
-                enumerable: false,
-            });
-        }
-        return obj;
-    };
-        const Q = fn => {
-        try {
-            return fn?.();
-        } catch {}
-    };
+    }
 })();
