592492871a57a852a687cef7e35c563d0d707ef0
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-09-07 21:54:42 -0500

    Update Record-duplex.js

--- Record-duplex.js
+++ Record-duplex.js
@@ -1,11 +1,22 @@
 (()=>{
     if (!typeof Request || !typeof Response || !typeof ReadableStream) return;
-    for(const record of [Request, Response, ReadableStream, Blob]){
-        Object.defineProperty(record.prototype, 'duplex', {
+    const Q = fn => {
+            try {
+                return fn?.();
+            } catch {}
+        };
+    const $global = Q(()=>globalThis) ?? Q(()=>self) ?? Q(()=>global) ?? Q(()=>window) ?? this;
+    const duplexHalf = x => Q(()=>Object.defineProperty(x, 'duplex', {
             value: 'half',
             configurable: true,
             writable: true,
             enumerable: false,
-        });
+        }));
+    for(const record of [Request, Response, ReadableStream, Blob]){
+        duplexHalf(record.prototype);
     }
+    (()=>{
+        const _Request = Request;
+        
+    })();
 })();
