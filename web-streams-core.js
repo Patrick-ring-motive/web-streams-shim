89e9eee9df57c671611175d09bfa1dc20993937d
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-11-13 18:53:53 -0600

    Update web-streams-core.js

--- web-streams-core.js
+++ web-streams-core.js
@@ -640,4 +640,42 @@ const instanceOf = (x, y) => Q(() => x instanceof y);
 
     }
 })();
+
+
+
+/**
+
+- Polyfill for the bytes() method on Request, Response, and Blob objects
+- Adds a bytes() method that returns a Uint8Array of the object’s content
+- @see https://fetch.spec.whatwg.org/#dom-body-bytes
+  */
+(() => {
+    // Apply bytes() method to Request, Response, and Blob prototypes
+    for (const record of [Q(() => Request), Q(() => Response), Q(() => Blob)]) {
+        (() => {
+            /**
+             * Returns the body/content as a Uint8Array
+             * Polyfill implementation that converts arrayBuffer() result to Uint8Array
+             * @returns {Promise<Uint8Array>} A promise that resolves to a Uint8Array containing the object’s bytes
+             * @note Sets Uint8Array as the prototype of the bytes function for better runtime type traceability
+             * @example
+             * // Usage with Response
+             * const response = new Response(‘hello’);
+             * const bytes = await response.bytes(); // Uint8Array
+             *
+             * // Usage with Blob
+             * const blob = new Blob([‘hello’]);
+             * const bytes = await blob.bytes(); // Uint8Array
+             *
+             * // Usage with Request
+             * const request = new Request(’/’, { method: ‘POST’, body: ‘data’ });
+             * const bytes = await request.bytes(); // Uint8Array
+             */
+            (record?.prototype ?? {}).bytes ??= extend(setStrings(async function bytes() {
+                return new Uint8Array(await this.arrayBuffer());
+            }), Q(() => Uint8Array) ?? {});
+        })();
+    }
+})();
+  
 })();
