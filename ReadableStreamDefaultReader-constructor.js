57c8d1496028399095e51c521747f56b64937108
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-11-14 19:40:10 -0600

    Update ReadableStreamDefaultReader-constructor.js

--- ReadableStreamDefaultReader-constructor.js
+++ ReadableStreamDefaultReader-constructor.js
@@ -1,3 +1,78 @@
+(()=>{
+const Q = fn => {
+        try {
+            return fn?.()
+        } catch {}
+    };
+    const constructPrototype = newClass => {
+        try {
+            if (newClass?.prototype) return newClass;
+            const constProto = newClass?.constructor?.prototype;
+            if (constProto) {
+                newClass.prototype = Q(() => constProto?.bind?.(constProto)) ?? Object.create(Object(constProto));
+                return newClass;
+            }
+            newClass.prototype = Q(() => newClass?.bind?.(newClass)) ?? Object.create(Object(newClass));
+        } catch (e) {
+            console.warn(e, newClass);
+        }
+    };
+    const extend = (thisClass, superClass) => {
+        try {
+            constructPrototype(thisClass);
+            constructPrototype(superClass);
+            Object.setPrototypeOf(
+                thisClass.prototype,
+                superClass?.prototype ??
+                superClass?.constructor?.prototype ??
+                superClass
+            );
+            Object.setPrototypeOf(thisClass, superClass);
+
+        } catch (e) {
+            console.warn(e, {
+                thisClass,
+                superClass
+            });
+        }
+        return thisClass;
+    };
+    /**
+
+    - Creates a function that returns a string for all string conversion methods
+    - Used to provide consistent string representations for polyfilled functions
+    - @param {string} str - The string to return
+    - @returns {Function} A function that returns the string for all conversion methods
+    - @private
+      */
+    const makeStringer = str => {
+        const stringer = () => str;
+        ['valueOf', 'toString', 'toLocaleString', Symbol.toPrimitive].forEach(x => {
+            stringer[x] = stringer;
+        });
+        stringer[Symbol.toStringTag] = str;
+        return stringer;
+    };
+
+    /**
+
+    - Sets string conversion methods on a function to indicate it’s polyfill code
+    - Provides consistent debugging experience by showing polyfill status
+    - @param {Function} obj - The function to modify
+    - @returns {Function} The modified function
+    - @private
+      */
+    const setStrings = (obj) => {
+        for (const str of ['toString', 'toLocaleString', Symbol.toStringTag]) {
+            Object.defineProperty(obj, str, {
+                value: makeStringer(`function ${obj.name}() { [polyfill code] }`),
+                configurable: true,
+                writable: true,
+                enumerable: false,
+            });
+        }
+        return obj;
+    };
 
 const supportsReadableStreamDefaultReaderConstructor = () => { 
   try {
@@ -14,3 +89,9 @@ const supportsReadableStreamDefaultReaderConstructor = () => {
     return false;
   }
 }
+
+  if(!supportsReadableStreamDefaultReaderConstructor()){
+    
+  }
+
+})();
