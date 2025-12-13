c9a0019525b136652caa729919b1ff1d368c83fc
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-12-12 19:50:16 -0600

    Update file.js

--- extensions/file.js
+++ extensions/file.js
@@ -1,33 +1,43 @@
-if(typeof File === 'undefined'){
-  // Sham File class extending Blob
-globalThis.File = class File extends Blob {
-  constructor(bits, filename, options = {}) {
-    // Extract File-specific options
-    const { lastModified = Date.now(), ...blobOptions } = options;
-    
-    // Call Blob constructor with bits and blob options
-    super(bits, blobOptions);
-    
-    // Add File-specific properties
-    this['&name'] = filename;
-    this['&lastModified'] = lastModified;
-    this['&lastModifiedDate'] = new Date(lastModified);
-  }
+(() => {
+    const Q = fn => {
+        try {
+            return fn?.()
+        } catch {}
+    };
+    const $global = Q(() => globalThis) ?? Q(() => global) ?? Q(() => self) ?? Q(() => window) ?? this;
+    if (typeof File === 'undefined') {
+        // Sham File class extending Blob
+        $global.File = class File extends Blob {
+            constructor(bits, filename, options = {}) {
+                // Extract File-specific options
+                const {
+                    lastModified = Date.now(), ...blobOptions
+                } = options;
 
-  get name() {
-    return this['&name'];
-  }
+                // Call Blob constructor with bits and blob options
+                super(bits, blobOptions);
 
-  get lastModified() {
-    return this['&lastModified'];
-  }
+                // Add File-specific properties
+                this['&name'] = filename;
+                this['&lastModified'] = lastModified;
+                this['&lastModifiedDate'] = new Date(lastModified);
+            }
 
-  get lastModifiedDate() {
-    return this['&lastModifiedDate'];
-  }
+            get name() {
+                return this['&name'];
+            }
 
-  get webkitRelativePath() {
-    return '';
-  }
-}
-}
+            get lastModified() {
+                return this['&lastModified'];
+            }
+
+            get lastModifiedDate() {
+                return this['&lastModifiedDate'];
+            }
+
+            get webkitRelativePath() {
+                return '';
+            }
+        }
+    }
+})();
